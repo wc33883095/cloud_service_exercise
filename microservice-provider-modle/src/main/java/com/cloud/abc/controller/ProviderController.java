c1521f02907d8c4430829041ce0217e6c48531cb
wc33883095 <32499009@qq.com>
2022-01-21 14:50:21 +0800

    【title】provider model【creater】杯浅巷子短

--- microservice-provider-modle/src/main/java/com/cloud/abc/controller/ProviderController.java
+++ microservice-provider-modle/src/main/java/com/cloud/abc/controller/ProviderController.java
@@ -1,4 +1,15 @@
 package com.cloud.abc.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class ProviderController {
+    @GetMapping("/get/{id}")
+    public String get(@PathVariable String id) {
+        return id;
+    }
 }
